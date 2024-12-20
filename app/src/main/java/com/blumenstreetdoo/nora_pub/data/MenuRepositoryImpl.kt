3cd07737bfb014f830cdcd99d5e96f90441d5a03
Iuliia Basalygina <korneva074@gmail.com>
2024-12-20 14:47:03 +0100

    Moved craftList mock to Menu repository

--- app/src/main/java/com/blumenstreetdoo/nora_pub/data/MenuRepositoryImpl.kt
+++ app/src/main/java/com/blumenstreetdoo/nora_pub/data/MenuRepositoryImpl.kt
@@ -1,6 +1,83 @@
 package com.blumenstreetdoo.nora_pub.data
 
 import com.blumenstreetdoo.nora_pub.domain.api.MenuRepository
+import com.blumenstreetdoo.nora_pub.domain.models.Beer
+import com.blumenstreetdoo.nora_pub.domain.models.Brewery
+import com.blumenstreetdoo.nora_pub.domain.models.DrinkType
+import com.blumenstreetdoo.nora_pub.domain.models.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class MenuRepositoryImpl : MenuRepository {
+    override fun getCraftList(): Flow<Resource<List<Beer>>> = flow {
+        emit(Resource.Success(generateMockCraftList())) // add ?: emptyList()
+    }
+
+    override fun getBeerById(id: String): Resource<Beer> {
+        val event = generateMockCraftList().find { it.id == id }
+        return event?.let { Resource.Success(it) } ?: Resource.Error("Event not found")
+    }
+
+    private fun generateMockCraftList(): List<Beer> = listOf(
+        Beer(
+            id = "1",
+            name = "IIPPAA",
+            type = DrinkType.TAP_BEER,
+            description = "IIPPAA is the best!",
+            price = 6.0,
+            volume = 500,
+            abv = 8.1,
+            imageUrl = "example",
+            brewery = Brewery(
+                id = "1",
+                name = "TOP beer",
+                country = "Serbia",
+                city = "Beograd",
+                url = "example"
+            ),
+            beerIbu = 60,
+            beerStyleId = "2",
+            beerStyle = "IPA"
+        ),
+        Beer(
+            id = "2",
+            name = "2 IIPPAA loashj asfjh ;sljdh ;dlfhg ;sdhk",
+            type = DrinkType.TAP_BEER,
+            description = "2 IIPPAA is the best!",
+            price = 6.0,
+            volume = 500,
+            abv = 8.1,
+            imageUrl = "example",
+            brewery = Brewery(
+                id = "1",
+                name = "TOP beer",
+                country = "Serbia",
+                city = "Beograd",
+                url = "example"
+            ),
+            beerIbu = 60,
+            beerStyleId = "2",
+            beerStyle = "Berliner Weisse"
+        ),
+        Beer(
+            id = "3",
+            name = "3 IIPPAA",
+            type = DrinkType.CANNED_BEER,
+            description = "3 IIPPAA is the best!",
+            price = 6.5,
+            volume = 500,
+            abv = 8.1,
+            imageUrl = "example",
+            brewery = Brewery(
+                id = "1",
+                name = "TOP beer",
+                country = "Serbia",
+                city = "Beograd",
+                url = "example"
+            ),
+            beerIbu = 60,
+            beerStyleId = "2",
+            beerStyle = "IPA"
+        ),
+    )
 }
--- app/src/main/java/com/blumenstreetdoo/nora_pub/ui/craft/CraftViewModel.kt
+++ app/src/main/java/com/blumenstreetdoo/nora_pub/ui/craft/CraftViewModel.kt
@@ -2,15 +2,15 @@ package com.blumenstreetdoo.nora_pub.ui.craft
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.blumenstreetdoo.nora_pub.domain.models.Beer
-import com.blumenstreetdoo.nora_pub.domain.models.Brewery
-import com.blumenstreetdoo.nora_pub.domain.models.DrinkType
-import kotlinx.coroutines.delay
+import com.blumenstreetdoo.nora_pub.domain.api.MenuInteractor
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
-class CraftViewModel : ViewModel() {
+class CraftViewModel(
+    private val menuInteractor: MenuInteractor
+) : ViewModel() {
     private val _craftState = MutableStateFlow<CraftScreenState>(CraftScreenState.Loading)
     val craftState: StateFlow<CraftScreenState> = _craftState
 
@@ -21,74 +21,16 @@ class CraftViewModel : ViewModel() {
     private fun getFullBeerList() {
         _craftState.value = CraftScreenState.Loading
         viewModelScope.launch {
-            delay(1000)
-            val fullBeerList = getMockBeerList()
-            _craftState.value = CraftScreenState.Content(fullBeerList)
+            try {
+                val fullBeerList = menuInteractor.getCraftList().first().first
+                if (fullBeerList != null) {
+                    _craftState.value = CraftScreenState.Content(fullBeerList)
+                } else {
+                    _craftState.value = CraftScreenState.Error("Failed to load data")
+                }
+            } catch (e: Exception) { // add NoInternet
+                _craftState.value = CraftScreenState.Error(e.message ?: "Unknown error")
+            }
         }
-    }
-
-    private fun getMockBeerList() : List<Beer> {
-        return listOf(
-            Beer(
-                id = "1",
-                name = "IIPPAA",
-                type = DrinkType.TAP_BEER,
-                description = "IIPPAA is the best!",
-                price = 6.0,
-                volume = 500,
-                abv = 8.1,
-                imageUrl = "example",
-                brewery = Brewery(
-                    id = "1",
-                    name = "TOP beer",
-                    country = "Serbia",
-                    city = "Beograd",
-                    url = "example"
-                ),
-                beerIbu = 60,
-                beerStyleId = "2",
-                beerStyle = "IPA"
-            ),
-            Beer(
-                id = "2",
-                name = "2 IIPPAA loashj asfjh ;sljdh ;dlfhg ;sdhk",
-                type = DrinkType.TAP_BEER,
-                description = "2 IIPPAA is the best!",
-                price = 6.0,
-                volume = 500,
-                abv = 8.1,
-                imageUrl = "example",
-                brewery = Brewery(
-                    id = "1",
-                    name = "TOP beer",
-                    country = "Serbia",
-                    city = "Beograd",
-                    url = "example"
-                ),
-                beerIbu = 60,
-                beerStyleId = "2",
-                beerStyle = "IPA"
-            ),
-            Beer(
-                id = "3",
-                name = "3 IIPPAA",
-                type = DrinkType.CANNED_BEER,
-                description = "3 IIPPAA is the best!",
-                price = 6.5,
-                volume = 500,
-                abv = 8.1,
-                imageUrl = "example",
-                brewery = Brewery(
-                    id = "1",
-                    name = "TOP beer",
-                    country = "Serbia",
-                    city = "Beograd",
-                    url = "example"
-                ),
-                beerIbu = 60,
-                beerStyleId = "2",
-                beerStyle = "IPA"
-            ),
-        )
     }
 }
